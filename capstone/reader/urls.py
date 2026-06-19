fb6571e3406d31e17db7b8c654f90ddeae559f22
Joy <christinejhoybernal@gmail.com>
2026-06-19 15:08:26 +0800

    Create initial pages with no content yet. and link each navigation in sidebar to each page

--- capstone/reader/urls.py
+++ capstone/reader/urls.py
@@ -3,5 +3,12 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index")
+    path("", views.index, name="index"),
+    path("discover", views.discover, name="discover"),
+    path("my-library", views.mylibrary, name="my-library"),
+    path("reading-lists", views.readinglists, name="reading-lists"),
+    path("saved-quote", views.savedquote, name="saved-quote"),
+    path("profile", views.profile, name="profile"),
+    path("settings", views.settings, name="settings"),
+
 ]
--- capstone/reader/views.py
+++ capstone/reader/views.py
@@ -3,3 +3,24 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, "reader/index.html")
+
+def discover(request):
+    return render(request, "reader/discover.html")
+
+def mylibrary(request):
+    return render(request, "reader/mylibrary.html")
+
+def readinglists(request):
+    return render(request, "reader/readinglists.html")
+
+def savedquote(request):
+    return render(request, "reader/savedquote.html")
+
+def profile(request):
+    return render(request, "reader/profile.html")
+
+def settings(request):
+    return render(request, "reader/settings.html")
+
+
+    
