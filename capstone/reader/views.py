f58fdaf121c49244e5f43fa59012b38ecd3ee948
Joy <christinejhoybernal@gmail.com>
2026-06-21 20:30:41 +0800

    create the initial layout of the profile page with mock data

--- capstone/reader/views.py
+++ capstone/reader/views.py
@@ -92,7 +92,82 @@ def savedquote(request):
     return render(request, "reader/savedquote.html")
 
 def profile(request):
-    return render(request, "reader/profile.html")
+    mock_books = [
+        {
+            "title": "Home",
+            "author": "Joy Alinsky",
+            "genre": "romance",
+            "reads": "15M",
+            "image": "reader/home.png"
+        },
+        {
+            "title": "Home",
+            "author": "Joy Alinsky",
+            "genre": "romance",
+            "reads": "15M",
+            "image": "reader/home.png"
+        },
+        {
+            "title": "Home",
+            "author": "Joy Alinsky",
+            "genre": "romance",
+            "reads": "15M",
+            "image": "reader/home.png"
+        },
+        {
+            "title": "Home",
+            "author": "Joy Alinsky",
+            "genre": "romance",
+            "reads": "15M",
+            "image": "reader/home.png"
+        },
+        {
+            "title": "Home",
+            "author": "Joy Alinsky",
+            "genre": "romance",
+            "reads": "15M",
+            "image": "reader/home.png"
+        },
+        {
+            "title": "Home",
+            "author": "Joy Alinsky",
+            "genre": "romance",
+            "reads": "15M",
+            "image": "reader/home.png"
+        },
+        {
+            "title": "Home",
+            "author": "Joy Alinsky",
+            "genre": "romance",
+            "reads": "15M",
+            "image": "reader/home.png"
+        },
+        {
+            "title": "Home",
+            "author": "Joy Alinsky",
+            "genre": "romance",
+            "reads": "15M",
+            "image": "reader/home.png"
+        },
+    ]
+    
+    genres = [
+        {"genre": "romance", "icon": "bi-heart-fill"},
+        {"genre": "fantasy", "icon": "bi-stars"},
+        {"genre": "sci-fi", "icon": "bi-rocket-takeoff"},
+        {"genre": "mystery", "icon": "bi-search"},
+        {"genre": "thriller", "icon": "bi-lightning-charge"},
+        {"genre": "horror", "icon": "bi-ghost"},
+        {"genre": "historical fiction", "icon": "bi-hourglass-split"},
+        {"genre": "biography", "icon": "bi-person-badge"},
+        {"genre": "self-help", "icon": "bi-lightbulb"},
+        {"genre": "adventure", "icon": "bi-compass"}
+    ]
+
+    return render(request, "reader/profile.html", {
+        "books": mock_books,
+        "genres": genres,
+    })
 
 def settings(request):
     return render(request, "reader/settings.html")
